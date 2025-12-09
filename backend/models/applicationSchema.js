b3e5e9f446d46b67cc674b061f3dd3c6b1ac5bb5
mdjabedmollah <mdjabed2167@gmail.com>
2025-12-09 21:36:13 +0600

    application model

---
+++ backend/models/applicationSchema.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const applicationSchema=new mongoose.Schema({
+     jobId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Job",
+        required:true
+     },
+     userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+     },
+     appliedAt:{
+        type:Date,
+        default:Date.now
+     }
+
+},{timestamps:true})
