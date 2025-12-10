97c6d137ffeb8b81906836f9d0d5344ee330b600
mdjabedmollah <mdjabed2167@gmail.com>
2025-12-10 21:40:33 +0600

    auth error solve

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,34 +1,32 @@
-import jwt from 'jsonwebtoken'
-export const auth=(req,res,next)=>{
-    const authheader =req.headers.authorization
-    if(!authheader|| !req.statsWith("Bearer "))
-        return res.status(401).json({
-    message:"No token ,authorization denied"
-    })
-    const token =authheader.split(" ")[1];
-    try {
-        const decoded=jwt.verify(token,process.env.JWT_SECRET)
-        req.user =decoded; // {id,role}
-        next()
-    } catch (error) {
-        return res.status(401).json({
-            message:"Token is not valid"
-        })
-    }
-    
-}
-export const isAdmin =(req,res,next)=>{
-   try {
-     if(req.user.role!=="admin"){
-        return res.status(401).json({
-    message:"Only admin allowed"
-    })
-    next()
-}
-   } catch (error) {
-     return res.status(401).json({
-            message:"Token is not valid"
-        })
-   }
-}
+import jwt from 'jsonwebtoken';
 
+export const auth = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      message: "No token, authorization denied",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // {id, role}
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Token is not valid",
+    });
+  }
+};
+
+export const isAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(401).json({
+      message: "Only admin allowed",
+    });
+  }
+  next();
+};
