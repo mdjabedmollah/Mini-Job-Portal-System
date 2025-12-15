9129bdd14b1af9dc8accd428777c308ce10bb214
mdjabedmollah <mdjabed2167@gmail.com>
2025-12-15 20:35:12 +0600

    cors

--- backend/index.js
+++ backend/index.js
@@ -17,14 +17,25 @@ DBconnection()
 //middleware 
 app.use(express.json())
 app.use(urlencoded({extended:true}))
-app.use(cors({
- origin: [
-      "http://localhost:5173",
-      "https://mini-job-portal-system.vercel.app",
-      "https://mini-job-portal-system-7gfnqun2k-mdjabed2167-gmailcoms-projects.vercel.app"
-    ],
-    credentials: true
-}))
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mini-job-portal-system.vercel.app",
+  "https://mini-job-portal-system-7gfnqun2k-mdjabed2167-gmailcoms-projects.vercel.app"
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // all api
 app.use('/api/auth',authRoute)
