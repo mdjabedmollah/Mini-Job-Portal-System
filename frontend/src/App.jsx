25b595389d8a0207441f8bb95321325e15cd7f70
mdjabedmollah <mdjabed2167@gmail.com>
2025-12-15 17:14:52 +0600

    footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import ProtectedRoute from "./components/ProtectedRoute";
+// import ProtectedRoute from "./components/ProtectedRoute";
 
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import JobList from "./pages/JobList";
-import JobDetails from "./pages/JobDetails";
-import AdminDashboard from "./pages/AdminDashboard";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import JobList from "./pages/JobList";
+// import JobDetails from "./pages/JobDetails";
+// import AdminDashboard from "./pages/AdminDashboard";
 
 const App = () => (
   <AuthProvider>
@@ -18,20 +18,20 @@ const App = () => (
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/register" element={<Register />} /> */}
 
-        <Route path="/jobs" element={
-          <ProtectedRoute><JobList /></ProtectedRoute>
-        } />
+        {/* <Route path="/jobs" element={ */}
+          {/* <ProtectedRoute><JobList /></ProtectedRoute> */}
+        {/* } /> */}
 
-        <Route path="/jobs/:id" element={
+        {/* <Route path="/jobs/:id" element={
           <ProtectedRoute><JobDetails /></ProtectedRoute>
         } />
 
         <Route path="/admin" element={
           <ProtectedRoute><AdminDashboard /></ProtectedRoute>
-        } />
+        } /> */}
       </Routes>
 
       <Footer />
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => (
+  <footer className="bg-gray-200 text-center py-3 mt-10">
+    <p className="text-sm text-gray-600">
+      © {new Date().getFullYear()} JobPortal
+    </p>
+  </footer>
+);
+
+export default Footer;
